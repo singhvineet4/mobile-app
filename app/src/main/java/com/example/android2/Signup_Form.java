c6c89eb3ecb343381092a27a79986687de308cb4
rajuk <rajuk@DESKTOP-75LD6C4>
2021-05-11 17:10:36 -0400

    updated login and register functionality

---
+++ app/src/main/java/com/example/android2/Signup_Form.java
@@ -0,0 +1,15 @@
+package com.example.android2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup_Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup__form);
+        getSupportActionBar().setTitle("Registration Form");
+    }
+}
